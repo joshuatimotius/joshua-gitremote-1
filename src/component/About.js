e0833dc8d940bc1d061e53f6d2e11cf504388f70
Joshua Timotius <joshuatimotius@gmail.com>
2020-02-28 09:32:47 +0700

    mengubah tampilan menu dari home ke about.js

--- src/component/About.js
+++ src/component/About.js
@@ -5,6 +5,18 @@ const About = () => {
     return (
         <div>
             <h2 className='greetings'>Welcome! This is the About Page</h2>
+            <h4 className='hometitle'>Games</h4>
+            <ul className="homelist">
+                <li>Apex Legends : 10</li>
+                <li>Counter Strike : 8</li>
+                <li>Dota 2 : 8</li>
+            </ul>
+            <h4 className='hometitle'>Songs</h4>
+            <ul className='homelist'>
+                <li>Another In The Fire : 9</li>
+                <li>Behold : 9</li>
+                <li>Sunday Morning : 8</li>
+            </ul>
             <p className='greetings'>this is everything you need to know about me....
                 no need to hesitate to <Link className='linkword' to='/contact'>contact</Link> me!
             </p>
--- src/component/Home.js
+++ src/component/Home.js
@@ -4,18 +4,6 @@ const Home = () => {
     return(
         <div>
             <h2 className='greetings'>Welcome! This is a Home Page</h2>
-            <h4 className='hometitle'>Games</h4>
-            <ul className="homelist">
-                <li>Apex Legends : 10</li>
-                <li>Counter Strike : 8</li>
-                <li>Dota 2 : 8</li>
-            </ul>
-            <h4 className='hometitle'>Songs</h4>
-            <ul className='homelist'>
-                <li>Another In The Fire : 9</li>
-                <li>Behold : 9</li>
-                <li>Sunday Morning : 8</li>
-            </ul>
         </div>
     );
 }
